3b754e63b6d48d1b83f40227f1195966e06f552d
THOMAS HARDING <hardingt604@gmail.com>
2022-05-03 15:34:47 -0600

    npm i, add initial state, add empty js of components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { useState, React } from 'react';
 import './App.css';
 
 function App() {
+  const [foodId, setFoodId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
+  const [sideId, setSideId] = useState(1);
+  const [orderName, setOrderName] = useState('three bears in a trenchcoat');
+  const [instructions, setInstructions] = useState(['no bread', 'salt on top']);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
